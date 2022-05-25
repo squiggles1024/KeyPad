8a4473015dab324762973cc9fc16fcfac7ef426e
Evan Lemson <evanlemson@gmail.com>
2022-05-25 16:17:20 -0700

    Added icon art assets

---
+++ Core/Inc/art_assets/art.h
@@ -0,0 +1,25 @@
+/*
+ * art.h
+ *
+ *  Created on: May 24, 2022
+ *      Author: evanl
+ */
+
+#ifndef INC_ART_ASSETS_ART_H_
+#define INC_ART_ASSETS_ART_H_
+#include <stdint.h>
+
+extern const uint8_t char_image[];
+extern const uint8_t bag_image[];
+extern const uint8_t mounts_image[];
+extern const uint8_t LFD_image[];
+extern const uint8_t spells_image[];
+extern const uint8_t talents_image[];
+extern const uint8_t social_image[];
+extern const uint8_t guild_image[];
+extern const uint8_t map_image[];
+extern const uint8_t achievements_image[];
+extern const uint8_t adventureguide_image[];
+extern const uint8_t menu_image[];
+
+#endif /* INC_ART_ASSETS_ART_H_ */
