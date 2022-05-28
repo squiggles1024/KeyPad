515d5b9977b493228f410d00e8a6d8559f14fa93
Evan Lemson <evanlemson@gmail.com>
2022-05-27 18:45:42 -0700

    Added I2C2 Support for Touch Screen

--- Core/Inc/i2c.h
+++ Core/Inc/i2c.h
@@ -34,14 +34,18 @@ extern "C" {
 
 extern I2C_HandleTypeDef hi2c1;
 
+extern I2C_HandleTypeDef hi2c2;
+
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
 
 void MX_I2C1_Init(void);
+void MX_I2C2_Init(void);
 
 /* USER CODE BEGIN Prototypes */
 void I2C1_Init(void);
+void I2C2_Init(void);
 /* USER CODE END Prototypes */
 
 #ifdef __cplusplus
