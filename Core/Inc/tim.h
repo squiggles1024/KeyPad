c938dc6265e5d7f89688e76999ff8ba514359126
Evan Lemson <evanlemson@gmail.com>
2022-05-27 18:48:12 -0700

    Added experimental Timer init function. Unused.

--- Core/Inc/tim.h
+++ Core/Inc/tim.h
@@ -44,6 +44,7 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
 
 /* USER CODE BEGIN Prototypes */
 void TIM3_Init(void);
+void TIM3_Init2(void);
 /* USER CODE END Prototypes */
 
 #ifdef __cplusplus
--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -212,4 +212,57 @@ void TIM3_Init(void){
 
 	  HAL_TIM_MspPostInit(&htim3);
 }
+
+void TIM3_Init2(){
+
+	  /* USER CODE BEGIN TIM3_Init 0 */
+
+	  /* USER CODE END TIM3_Init 0 */
+
+	  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+	  TIM_MasterConfigTypeDef sMasterConfig = {0};
+	  TIM_OC_InitTypeDef sConfigOC = {0};
+
+	  /* USER CODE BEGIN TIM3_Init 1 */
+
+	  /* USER CODE END TIM3_Init 1 */
+	  htim3.Instance = TIM3;
+	  htim3.Init.Prescaler = 0;
+	  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+	  htim3.Init.Period = 120 - 1;
+	  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+	  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+	  {
+	    Error_Handler();
+	  }
+	  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+	  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+	  {
+	    Error_Handler();
+	  }
+	  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+	  {
+	    Error_Handler();
+	  }
+	  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+	  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+	  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+	  {
+	    Error_Handler();
+	  }
+	  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+	  sConfigOC.Pulse = 0;
+	  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+	  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+	  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+	  {
+	    Error_Handler();
+	  }
+	  /* USER CODE BEGIN TIM3_Init 2 */
+
+	  /* USER CODE END TIM3_Init 2 */
+	  HAL_TIM_MspPostInit(&htim3);
+
+}
 /* USER CODE END 1 */
