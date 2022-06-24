c383fa996956c5499cb5e9e5d37817ad4b04d004
Evan Lemson <evanlemson@gmail.com>
2022-06-23 17:30:58 -0700

    Cube IDE Code Change. Ignore

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -50,13 +50,13 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, SPI1_CSN_Pin|DCRS_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOB, SPI1_CSN_Pin|DCRS_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : PBPin PBPin */
   GPIO_InitStruct.Pin = SPI1_CSN_Pin|DCRS_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
