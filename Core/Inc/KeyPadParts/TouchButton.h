8aa7a11c450ea9f18fd31a139ba2bdc5326cc21f
Evan Lemson <evanlemson@gmail.com>
2022-06-24 20:53:19 -0700

    Remove magic numbers, Convert to KeyFunction type

--- Core/Inc/KeyPadParts/TouchButton.h
+++ Core/Inc/KeyPadParts/TouchButton.h
@@ -11,6 +11,13 @@
 #include "TouchButton_Hardware.h"
 #include <stdint.h>
 
+#define TOUCH_BUTTON_HALFWIDTH (31) //62 x 62 Pixels
+#define TOUCH_BUTTONS_PER_ROW (4)
+#define TOUCH_BUTTON_ROW_PITCH (80)
+#define TOUCH_BUTTON_COLUMN_PITCH (60)
+#define TOUCH_BUTTON1_X_CENTER (40)
+#define TOUCH_BUTTON1_Y_CENTER (60)
+
 typedef enum{
     TouchButtonChanged,
 	TouchButtonUnchanged
@@ -19,26 +26,20 @@ typedef enum{
 typedef struct{
 	TouchButtonCoordinate_t TopLeftBound;
 	TouchButtonCoordinate_t BottomRightBound;
-	KeyScanCode KeyFunction;
-	ModifierKey ModKeyFunction;
-    MouseClick  MouseFunction;
-    MouseWheel  WheelFunction;
+	KeyFunction_t KeyFunction;
 }TouchButton_Init_Struct_t;
 
 typedef struct{
 	TouchButtonCoordinate_t TopLeftBound;
 	TouchButtonCoordinate_t BottomRightBound;
-	KeyScanCode KeyFunction;
-	ModifierKey ModKeyFunction;
-    MouseClick  MouseFunction;
-    MouseWheel  WheelFunction;
+	KeyFunction_t KeyFunction;
     TouchButtonState_t State;
     TouchButton_IO_Drv_t IO_Driver;
 }TouchButton_Handle_t;
 
 void TouchButtonInit(TouchButton_Init_Struct_t Settings, TouchButton_Handle_t *Dev, TouchButton_IO_Drv_t IO_Driver);
 TouchButton_Status_t TouchButtonRead(TouchButton_Handle_t *Dev);
-void TouchButtonSetFunction(TouchButton_Handle_t* Dev, KeyScanCode KeyFunc, ModifierKey ModKey, MouseClick MouseFunction, MouseWheel WheelFunction);
+void TouchButtonSetFunction(TouchButton_Handle_t* Dev, KeyFunction_t NewFunction);
 
 
 
